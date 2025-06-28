3d39434f22bad89f59caeddf35025440f98dd05c
Faiz Shaikh <142882988+Faiz2172@users.noreply.github.com>
2025-06-28 16:22:18 +0530

    Update api.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,3 +1,3 @@
 // if you're using your physical phone, change this to the deployed url
-// we have explained this in the course :-)
+// if you are not having andriod studio try using the ipaddress of your pc inplace of localhost
 export const API_URL = "http://localhost:5001/api";
